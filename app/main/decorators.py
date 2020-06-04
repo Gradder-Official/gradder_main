60ddaf3a4e10756c5ae0fba79067ab1ef197d26f
Alex Nikanov <alekseynikanov.21@gmail.com>
2020-06-04 08:51:22 -0400

    Updated Access Level Decorators
    Access level Decorators won't crash if a user is not logged in.

--- app/main/decorators.py
+++ app/main/decorators.py
@@ -7,7 +7,7 @@ def required_access(people):
     def iteration(func):
         @wraps(func)
         def decorated_function(*args, **kwargs):
-            if current_user.USERTYPE not in people:
+            if not current_user.is_authenticated or current_user.USERTYPE not in people:
                 flash("You do not have access to this page! Please check your login info.")
                 print('dont have access')
                 return redirect(url_for('auth.login'))
