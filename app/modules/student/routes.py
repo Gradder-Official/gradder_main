2ba2ac853a663e9b2fa1061502c59f46e2c08583
shreygupta04 <shreygupta04@gmail.com>
2020-07-03 18:44:13 -0400

    Submission uses dynamic URLs

--- app/modules/student/routes.py
+++ app/modules/student/routes.py
@@ -1,12 +1,14 @@
 from flask import render_template, redirect, request, url_for, make_response
 from flask_login import current_user
-from app.modules.student.forms import SubmissionForm
+from app.modules.teacher.forms import NewSubmissionForm
 from . import student
 from ._student import Student
 from app.modules._classes import Submission
 from app.decorators import required_access
 from datetime import datetime
 from app.google_storage import upload_blob
+from app import db
+from bson import ObjectId
 
 @student.before_request
 @required_access('Student')
@@ -21,9 +23,14 @@ def student_verification():
 def index():
     return render_template('student/dashboard.html', assignments=current_user.get_assignments())
 
-@student.route('/submit', methods=["GET", "POST"])
-def submit():
-    form = SubmissionForm()
+@student.route('/submit/<class_id>/<assignment_id>', methods=["GET", "POST"])
+def submit(class_id, assignment_id):
+    form = NewSubmissionForm()
+    assignment = db.classes.find_one({"assignments._id": ObjectId(assignment_id)}, {"_id": 0, "assignments": {"$elemMatch": {"_id": ObjectId(assignment_id)}}})["assignments"][0]
+    full_name = current_user.first_name + " " +  current_user.last_name
+    content = assignment["content"]
+    estimated_time = assignment["estimated_time"]
+    due_by = assignment["due_by"]
     if form.validate_on_submit():
         student = Student.get_by_id(current_user.ID)
         if request.files is not None:
@@ -32,9 +39,10 @@ def submit():
             for file_ in files:
                 blob = upload_blob('gradder-storage', file_.filename, file_)
                 file_link_list.append(blob.media_link)
-        submission = Submission(date_submitted=datetime.utcnow(), comment=form.comment.data, file_links=file_link_list)
-        student.add_submission("5efbe85b4aeb5d21e56fa81f", "5efdfc905bc672bf745e4580", submission=submission)
-    return render_template('student/submission.html', form=form)
+        submission = Submission(date_submitted=datetime.utcnow(), content=form.content.data, file_links=file_link_list)
+        student.add_submission(class_id, assignment_id, submission=submission) # need to replace IDs with current class and assignment ID
+    return render_template('student/submission.html', form=form, class_id=class_id, assignment_id=assignment_id, full_name=full_name, 
+                        content=content, estimated_time=estimated_time, due_by=due_by)
 
 @student.route('/profile')
 def profile():
