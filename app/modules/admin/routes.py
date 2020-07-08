5ae75edc914302a86daf9367510eb46d289a66b6
karthikb19 <karthikmbhargav@gmail.com>
2020-07-08 11:14:48 -0400

    Admins can add new classes, also updated Admin Dashboard

--- app/modules/admin/routes.py
+++ app/modules/admin/routes.py
@@ -1,7 +1,19 @@
-from flask import render_template, redirect, request, url_for
+from datetime import datetime
+from flask import redirect, url_for, flash, render_template, request
+from flask_login import current_user
+from werkzeug.utils import secure_filename
 
 from . import admin
+
 from ._admin import Admin
+from app.modules._classes import Classes
+
+from app.decorators import required_access
+from app.google_storage import upload_blob
+from app.modules._classes import Assignment, Classes
+from app.logs.form_logger import form_logger
+import uuid
+# from app.modules.admin._admin import add_student
 
 from app.decorators import required_access
 
@@ -21,4 +33,14 @@ def index():
 
 @admin.route('/profile')
 def profile():
+    # add_student("912f58eccfe825c85801", "coolgm@gmail.com")
     return render_template('admin/profile.html')
+
+
+# @admin.route('/class', methods=['GET'])
+# def manage_classes():
+#     return redirect(url_for('admin.manage_classes_by_id', class_id=current_user.get_class_names()[0][0]))
+
+# @admin.route('/class/<string:class_id>', methods=['GET'])
+# def manage_classes_by_id(class_id: str):
+#     return render_template('/admin/manage_classes.html', classes=current_user.get_class_names(), class_json=Classes.get_by_id(class_id).to_json())
