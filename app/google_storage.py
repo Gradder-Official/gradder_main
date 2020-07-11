51c8e562d25c7caa274a06d22615c5c3203fc7ce
shreygupta04 <shreygupta04@gmail.com>
2020-07-10 21:12:00 -0400

    Assignments now open in browser

--- app/google_storage.py
+++ app/google_storage.py
@@ -1,3 +1,5 @@
+import tempfile
+import os
 from google.cloud import storage
 from app.logs.form_logger import form_logger
 
@@ -14,3 +16,10 @@ def download_blob(filename, actual_filename):
     bucket = storage_client.bucket('gradder-storage')
     blob = bucket.blob(filename)
     blob.download_to_filename(actual_filename)
+
+def download_blob_binary(filename, actual_filename):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('gradder-storage')
+    blob = bucket.blob(filename)
+    binary_obj = blob.download_as_string()
+    return binary_obj
