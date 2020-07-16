8d549dc052094983fac2654852d558d115ca99a8
Alex Nikanov <alekseynikanov.21@gmail.com>
2020-07-16 19:22:48 +0300

    Merged system-design into dev

--- app/decorators.py
+++ app/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import redirect, url_for, flash
+from flask import redirect, url_for, flash, abort
 from flask_login import current_user
 from app.logger import logger
 
@@ -8,9 +8,7 @@ def required_access(people):
         @wraps(func)
         def decorated_function(*args, **kwargs):
             if not current_user.is_authenticated or current_user.USERTYPE not in people:
-                flash("You do not have access to this page! Please check your login info.")
-                logger.info("User tried to access unauthorized page")
-                return redirect(url_for('auth.login'))
+                abort(403)
             return func(*args, **kwargs)
         return decorated_function
     return iteration
