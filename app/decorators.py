5a143498cd886657e56273c47a1f9aea2e1132b1
Alex Nikanov <alekseynikanov.21@gmail.com>
2020-07-16 19:15:30 +0300

    Slightly better error pages

--- app/decorators.py
+++ app/decorators.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import redirect, url_for, flash
+from flask import redirect, url_for, flash, abort
 from flask_login import current_user
 from app.logs.user_logger import user_logger
 
@@ -8,8 +8,7 @@ def required_access(people):
         @wraps(func)
         def decorated_function(*args, **kwargs):
             if not current_user.is_authenticated or current_user.USERTYPE not in people:
-                flash("You do not have access to this page! Please check your login info.")
-                return redirect(url_for('auth.login'))
+                abort(403)
             return func(*args, **kwargs)
         return decorated_function
     return iteration
