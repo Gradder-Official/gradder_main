3b09e20912ff37ae799a640887e97571e54ac5c6
Alex Nikanov <alekseynikanov.21@gmail.com>
2020-07-17 18:07:20 +0300

    Added Dockerfile and fixed some logger bugs

--- debug.py
+++ debug.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app("development")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
