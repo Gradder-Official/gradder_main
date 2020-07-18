7c8d81125b5c9b819b878f73f8d1baeb0e46c5ae
karthikb19 <karthikmbhargav@gmail.com>
2020-07-18 12:35:20 -0400

    Adding tests

---
+++ tests/test_admin.py
@@ -0,0 +1,51 @@
+import unittest
+from flask import current_app
+from app import create_app
+import os
+
+from mockupdb import MockupDB, go, Command
+from pymongo import MongoClient
+from json import dumps
+
+from app.modules.admin import routes
+
+class AdminTests(unittest.TestCase):
+    r"""
+
+    Tests for Admin
+
+    """
+
+    # Test Case standard stuff
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    # helper function   
+    def dashboard(self, usertype):
+        return self.client.get('/dashboard', follow_redirects=True)
+    
+    def register_classes(self, department, number, name, teacher, description, schedule_time, schedule_days):
+        return self.client.post('/registerClasses', data=dict(
+            department=department,
+            number=number,
+            name=name,
+            teacher=teacher,
+            description=description,
+            schedule_time=schedule_time,
+            schedule_days=schedule_days
+        ), follow_redirects=True)
+    # Checking register classes
+
+    def test_dashboard_loading(self):
+        response = self.client.get('/dashboard')
+        self.assertEqual(response.status, '302 FOUND', 'Dashboard route does not redirect')
+
+    def registerClasses(self):
+        response = self.register_classes('CS', '321n', 'Game Theory in CS', '5f016d7d0fcc27aa6bdffe43', 'Learning Game Theory', '09:10', '3, 4')
+        self.assertTrue(response, "Registering Classes worked")
