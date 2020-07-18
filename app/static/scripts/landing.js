b5e5fe2ec5ab5634684403a3a3f940c3766bc40c
Michelle <lomichelle42@gmail.com>
2020-07-18 15:01:53 +0800

    fixed landing page

--- app/static/scripts/landing.js
+++ app/static/scripts/landing.js
@@ -99,52 +99,13 @@ indicator3.onclick = function () {
     toThree();
 }
 
-/* Показать текст
-document.getElementsByClassName('custom-options')[0].addEventListener('click', function() {
-	document.getElementById('problem').style.opacity = "1";
-	document.getElementById('problem').style.pointerEvents = "auto";
-	document.getElementById('problemLink').style.color = "#212121";
-
-	document.getElementById('solution').style.opacity = "0";
-	document.getElementById('solution').style.pointerEvents = "none";
-	document.getElementById('solutionLink').style.color = "#A3A3A3";
+/* Navigation */
+var scrollspyNav = document.getElementById("scrollspy-nav");
+$(document).scroll(function () {
+    var y = $(this).scrollTop();
+    if (y > 600) {
+        scrollspyNav.style.opacity = 1;
+    } else {
+        scrollspyNav.style.opacity = 0;
+    }
 });
-
-function showText(text){
-	if (text == 'solution') {
-		document.getElementById(text).style.opacity = "1";
-		document.getElementById(text).style.pointerEvents = "auto";
-		document.getElementById('solutionLink').style.color = "#212121";
-
-		document.getElementById('problem').style.opacity = "0";
-		document.getElementById('problem').style.pointerEvents = "none";
-		document.getElementById('problemLink').style.color = "#A3A3A3";
-	} else if (text == 'problem') {
-		document.getElementById(text).style.opacity = "1";
-		document.getElementById(text).style.pointerEvents = "auto";
-		document.getElementById('problemLink').style.color = "#212121";
-
-		document.getElementById('solution').style.opacity = "0";
-		document.getElementById('solution').style.pointerEvents = "none";
-		document.getElementById('solutionLink').style.color = "#A3A3A3";
-	}
-}
-
-/* Открыть ссылки
-function linkOpen(url, type) {
-	if (type == 'blank') {
-  		window.open(url); // в новой вкладке
-	} else if (type == 'curr') {
-  		window.location.assign(url); // в текущем окне
-	}	
-}
-
-function outsideOnclick() {
-   let langScreen = document.getElementsByClassName('custom-options')[0];
-   let langBtn = document.getElementsByClassName('custom-select-trigger')[0];
-
-   if (event.target != langScreen && event.target != langBtn) {
-      document.getElementsByClassName('custom-options')[0].style.opacity = "0";
-      langAppear('none', 70);
-   }
-} */
