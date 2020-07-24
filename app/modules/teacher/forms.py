b669cb7e71f012b4ebb0e4eeaf7750179703275e
discordstars <19372006+discordstars@users.noreply.github.com>
2020-07-24 19:45:13 +0100

    edit and display individual assignment route for teachers

--- app/modules/teacher/forms.py
+++ app/modules/teacher/forms.py
@@ -1,15 +1,10 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import (
-    DateTimeField,
-    StringField,
-    TextAreaField,
-    SubmitField,
-    SelectField,
-    MultipleFileField,
-)
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from wtforms import (DateTimeField, MultipleFileField, SelectField,
+                     StringField, SubmitField, TextAreaField)
 from wtforms.validators import DataRequired
 
+from app.mixins import JSONForm
 from bson import ObjectId
 
 
@@ -22,6 +17,25 @@ class NewAssignmentForm(FlaskForm):
         "Estimated time in minutes", validators=[DataRequired()]
     )
     title = StringField("Assignment title", validators=[DataRequired()])
+    content = TextAreaField("The assignment description")
+
+    files = MultipleFileField(
+        "File upload",
+        validators=[
+            FileAllowed(
+                ["pdf", "docx", "png", "jpg", "jpeg"],
+                "Allowed formats: pdf, docx, png, jpeg",
+            )
+        ],
+    )
+
+    submit = SubmitField("Submit")
+
+class EditAssignmentForm(FlaskForm, JSONForm):
+    assigned_to = SelectField(u"Class", coerce=ObjectId)
+    due_by = StringField("Due by")  # Not DateTime field because it is weird
+    estimated_time = StringField("Estimated time in minutes")
+    title = StringField("Assignment title")
     content = TextAreaField("The assignment description")
 
     files = MultipleFileField(
@@ -58,4 +72,3 @@ class EditClassForm(FlaskForm):
     syllabus = FileField("Update syllabus (current: empty)")
 
     submit = SubmitField("Submit")
-
