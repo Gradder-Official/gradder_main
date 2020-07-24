6c3564e9f5d95d9e3dcec9b0195bf8a8d62ccf1c
discordstars <19372006+discordstars@users.noreply.github.com>
2020-07-24 11:22:20 +0100

    Restructure JSONForm.get_form_json This also adds 'type' support, including non-standard types such as textarea

--- app/mixins.py
+++ app/mixins.py
@@ -1,23 +1,40 @@
-from wtforms.fields import Field
+from wtforms.fields.core import Field
+from wtforms.widgets import TextArea, Select, Option
 
 class JSONForm:
     def get_form_json(self):
-        return [
-            {
-                'id': field.id,
-                'name': field.short_name,
-                'label': {
-                    'field_id': field.label.field_id,
-                    'text': field.label.text,
-                },
-                'flags': [
-                    {'name': flag_name, 'value': flag_value}
-                    for flag_name, flag_value in field.flags.__dict__.items()
-                ],
-                'default': field.default,
-                'description': field.description,
-                'value': field._value() if hasattr(field, '_value') else None
-            }
-            for name, field in self.__dict__.items()
-            if issubclass(field.__class__, Field)
-        ]
+        fields = []
+        for name, field in self.__dict__.items():
+            if issubclass(field.__class__, Field):
+                # Attributes we know definitely exist
+                field_data = {
+                    'id': field.id,
+                    'name': field.short_name,
+                    'label': {
+                        'field_id': field.label.field_id,
+                        'text': field.label.text,
+                    },
+                    'flags': [
+                        {'name': flag_name, 'value': flag_value}
+                        for flag_name, flag_value in field.flags.__dict__.items()
+                    ],
+                    'default': field.default,
+                    'description': field.description,
+                    'value': None,
+                    'type': 'text',
+                }
+
+                if hasattr(field, '_value'):
+                    field_data['value'] = field._value()
+                
+                if hasattr(field, 'widget'):
+                    if hasattr(field.widget.__class__, 'input_type'):
+                        field_data['type'] = field.widget.__class__.input_type
+                    else:
+                        # If a special type, just use name
+                        # This only applies for TextArea, Select and Option
+                        field_data['type'] = field.widget.__class__.__name__.lower()
+
+                fields.append(field_data)
+
+        return fields
