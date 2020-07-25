1cf2aab18465624c5563afe70d687d04a213292d
Lakshita Jain <Lak.Jai110@gmail.com>
2020-07-25 08:56:08 +0200

    Removed mixin

--- app/mixins.py
+++ app/mixins.py
@@ -44,8 +44,3 @@ class JSONForm:
         return fields
 
     
-class JSONEncoder(json.JSONEncoder):
-        def default(self, o):
-            if isinstance(o, ObjectId):
-                return str(o)
-            return json.JSONEncoder.default(self, o)
