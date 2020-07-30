e92ef77c6b82fda0b5e3ffa93e4aa50b9bad56cf
Alex Nikanov <alekseynikanov.21@gmail.com>
2020-07-30 16:06:29 +0300

    feat(all): Added blueprint initializations

---
+++ api/modules/parent/__init__.py
@@ -0,0 +1,10 @@
+r"""The blueprint that handles all the parent-related things.
+This includes parent dashboard, communication with teachers, and more.
+"""
+from flask import Blueprint
+
+parent = Blueprint(
+    "parent",
+    __name__,
+    url_prefix="/api/parent",
+)
---
+++ api/modules/teacher/__init__.py
@@ -0,0 +1,10 @@
+r"""The blueprint that handles all the teacher-related things.
+This includes managing classes, posting assignments, viewing students, and more.
+"""
+from flask import Blueprint
+
+teacher = Blueprint(
+    "teacher",
+    __name__,
+    url_prefix="/api/teacher",
+)
