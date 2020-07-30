10e3db80f8c989c3cd9207412db7ae5627845bc7
Michelle <lomichelle42@gmail.com>
2020-07-30 14:47:18 +0800

    feat: :sparkles: Rewrote responsive student dashboard in React with sidebar component
    Created student dashboard with empty containers and working student sidebar. Styled to be mobile friendly and responsive.

--- client/src/components/StudentSidebar.tsx
+++ client/src/components/StudentSidebar.tsx
@@ -1,33 +1,39 @@
 import React, { FunctionComponent } from 'react'
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import WhiteLogo from "../assets/images/White Logo.png"
 import "../assets/styles/sidebar.css"
-
 
 const StudentSidebar: FunctionComponent = () => {
     return (
         <div className="dash-sidebar">
             <div className="navbar-header">
-                <Link to="/dashboard" className="sidebar-logo">
+                <NavLink to="/dashboard" className="sidebar-logo">
                     <img src={ WhiteLogo } alt="Gradder" />
                     <h4>Gradder</h4>
-                </Link>
+                </NavLink>
             </div>
             <nav className="nav flex-column nav-pills" id="sidebar-nav">
-                <Link to="/dashboard" className="nav-link active" >Overview</Link>
-                <Link to="/dashboard" className="nav-link" >Timetable</Link>
-                <Link to="/student/assignments" className="nav-link">Assignments</Link>
-                <Link to="/dashboard" className="nav-link">Analytics</Link>
-                <Link to="/profile" className="nav-link">Profile</Link>
+                <NavLink to="/student/dashboard" className="nav-link" activeClassName="active">
+                    <span className="material-icons-outlined">home</span>
+                    Overview
+                </NavLink>
+                <NavLink to="/dashboard" className="nav-link" activeClassName="active">
+                    <span className="material-icons-outlined">calendar_today</span>
+                    Timetable
+                </NavLink>
+                <NavLink to="/student/assignments" className="nav-link" activeClassName="active">
+                    <span className="material-icons-outlined">check_box</span>
+                    Assignments
+                </NavLink>
+                <NavLink to="/dashboard" className="nav-link" activeClassName="active">
+                    <span className="material-icons-outlined">pie_chart</span>   
+                    Analytics
+                </NavLink>
+                <NavLink to="/profile" className="nav-link" activeClassName="active">
+                    <span className="material-icons-outlined">settings</span>
+                    Settings
+                </NavLink>
             </nav>
-            <div className="navbar-bottom">
-                <Link to="/dashboard">
-                    <i className="material-icons">mail</i>
-                </Link>
-                <Link to="/">
-                    <i className="material-icons">exit_to_app</i>
-                </Link>
-            </div>
         </div>
     )
 }
