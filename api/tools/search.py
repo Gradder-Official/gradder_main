39b8d190401190060b54fc3ee980aef27c1c9ace
discordstars <19372006+discordstars@users.noreply.github.com>
2020-08-12 11:55:13 +0100

    feat: :sparkles: Search utility

---
+++ api/tools/search.py
@@ -0,0 +1,40 @@
+from operator import attrgetter
+
+
+def get(iterable, **kwargs: dict):
+    r"""Find an element of ``iterable`` with the attributes
+    specified in kwargs. For example:
+
+        get(assignments, id='an id here') # >>> <Assignment...>
+
+    Also supports nested attributes:
+
+        # Get assignments that last 100 minutes
+        get(assignments, due_by__total_seconds=100*60)
+
+    Parameters
+    ----------
+    iterable : any
+        An iterable to search through
+    """
+    if len(kwargs) == 1:
+        # Most common use case
+        k, v = kwargs.popitem()
+        pred = attrgetter(k.replace('__', '.'))
+        for element in iterable:
+            if pred(element) == v:
+                return element
+    else:
+        # More complicated use case
+        # Convert nested attributes to dotted attributes
+        converted = [
+            (attrgetter(kwarg.replace('__', '.')), value)
+            for kwarg, value in kwargs.items()
+        ]
+
+        for element in iterable:
+            if all(pred2(element) == value for pred2, value in converted):
+                return element
+    
+    # Oops, nothing was found :c
+    return None
