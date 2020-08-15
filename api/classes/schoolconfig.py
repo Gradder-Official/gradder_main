244a716be2c133a456ab74e4c56493e71bc564c1
Lakshita Jain <Lak.Jai110@gmail.com>
2020-08-15 22:08:56 +0200

    Added SchoolConfig class

---
+++ api/classes/schoolconfig.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from api import db
+from api import root_logger as logger
+
+class SchoolConfig: 
+    # Add attributes and methods to edit db.general_info
+    pass
