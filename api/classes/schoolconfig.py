38e4ee58d4c0a2decd61f81d544f482350169a20
karthikb19 <karthikmbhargav@gmail.com>
2020-08-16 20:33:16 -0400

    Setting up basic configuration for School Config

--- api/classes/schoolconfig.py
+++ api/classes/schoolconfig.py
@@ -3,6 +3,57 @@ from __future__ import annotations
 from api import db
 from api import root_logger as logger
 
+
+# ToDO:
+# Add the proper methods for SchoolConfig
+# Do we need an ObjectId, and discuss other technical details
 class SchoolConfig: 
     # Add attributes and methods to edit db.general_info
-    pass
+    def __init__(
+        self,
+        school_name: str,
+        school_address: str,
+        phone_number: str,
+        school_email: str,
+        principal: str,
+        principal_email: str,
+        departments: List[str]
+        department_description: List[str],
+        grade_weights: List[bool],
+        grading: List[str]
+    )
+    """
+    Helps with SchoolConfig
+
+    Parameters
+    ------------
+    school_name: str
+        Updating the name of the school
+
+    school_address: str
+        Address of the school
+
+    phone_number: str,
+        Phone Number of the school
+
+    school_email: str,
+        Email address of the school
+
+    principal: str,
+        The Current principal at the school
+
+    principal_email: str,
+        Email of the current principal of the school
+    
+    departments: List[str],
+        A List of 3-letter abbreivations of the departments at the school
+    
+    department_description: List[str]
+        List of descriptions for each department in the school
+    
+    grade_weights: List[bool],
+        For each class in the school, decides if there is a weight or not(True == Weight, False == No Weight)
+    
+    grading: List[str],
+        Grading System for the school(Can be Letter Grades(A-F))
+    """
