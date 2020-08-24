ddfb660c3d7ffa81de0012e0db010b98ebbf047c
unknown <varunch321@gmail.com>
2020-08-24 01:38:09 -0400

    Fix responsiveness and design of the Teacher and Student profile page

--- client/src/pages/StudentProfile.tsx
+++ client/src/pages/StudentProfile.tsx
@@ -28,61 +28,56 @@ const StudentProfile: FunctionComponent<student> = ({ userName, dob }) => {
 
             {/*This is the mobile approach using React-Boostrap*/}
             <div className="dash-content" id="student-overview">
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col className="profileDetails" xs={12} sm={8}>
-                            <div className="dash-container profile">
-                                <div className="profile-details">
-                                    <h6>Welcome!</h6>
-                                    <h2>{userName}</h2>
-
-                                </div>
-                                <div className="profile-time">
-                                    <Link to="/">
-                                        <i className="material-icons-outlined">exit_to_app</i>
-                                    </Link>
-                                    <Link to="/dashboard">
-                                        <i className="material-icons-outlined">mail</i>
-                                    </Link>
-                                    <h1>{curTime}</h1>
-                                    <p>{curDate}</p>
-                                </div>
+                {/*This is the mobile approach using React-Boostrap*/}
+                <Container fluid className="d-block d-lg-none">
+                    {/*</Container><Row className="justify-content-md-center" */}
+                    <Row className="dash-container-small profileDetails-small" xs={12}>
+                        <Col className="profile-small" xs={12}>
+                            <div className="profile-details">
+                                <p className="h3">Welcome, <br></br>{userName}!</p>
+                            </div>
+                       </Col>
+                        <Col xs={12} className="profile-second-row">
+                            <div className="profile-time-small text-left text-white">
+                                <Link to="/">
+                                    <i className="text-left -mr-2 material-icons-outlined">exit_to_app</i>
+                                </Link>
+                                <Link to="/dashboard">
+                                    <i className="text-left material-icons-outlined">mail</i>
+                                </Link>
+                                <p className="h1">{curTime}</p>
+                                <p>{curDate}</p>
                             </div>
                         </Col>
-                        <Col xs={12} sm={8}>
-                            <img src="https://www.iambetter.org/wp-content/uploads/2020/04/Albert_Einstein_1024x1024.jpg" alt="Profile" className="profile-pic" />
-                        </Col>
-                        <Col xs={12} sm={8}>
-                            <div className="dash-container-small">
-                                <h5>Age</h5>
-                                <p>{ageFromString}</p>
-                            </div>
-                        </Col>
-                        <Col xs={12} sm={8}>
-                            <div className="dash-container-small">
-                                <h5>Date of Birth</h5>
-                                <p>{dob}</p>
-                            </div>
-                        </Col>
-                        <Col xs={12} sm={8}>
+                    </Row>
+                    <Row xs={12}>
+                        <img src="https://www.iambetter.org/wp-content/uploads/2020/04/Albert_Einstein_1024x1024.jpg" alt="Profile" id="profile-pic-small" className="profile-pic" />
+                    </Row>
+                    <Row xs={12}>
+                        <div className="dash-container-small">
+                            <h5>Date of Birth</h5>
+                            <p>{dob}</p>
+                        </div>
+                    </Row>
+                    <Row xs={12}>
                         <div className="dash-container timetable">
                             <h5>Change Password</h5>
                         </div>
-                        </Col>
                     </Row>
+                    {/* </Row> */}
                 </Container>
 
                 {/*This is the desktop/large screen version*/}
 
-                
-                {/*
-                <div className="student-dash-flex-row">
+
+
+                <div className="student-dash-flex-row d-none d-lg-flex">
                     <div className="student-dash-flex-col">
                         <div className="dash-container profile">
                             <div className="profile-details">
                                 <h6>Welcome!</h6>
                                 <h2>{userName}</h2>
-                                
+
                             </div>
                             <div className="profile-time">
                                 <Link to="/">
@@ -111,10 +106,8 @@ const StudentProfile: FunctionComponent<student> = ({ userName, dob }) => {
                         </div>
                     </div>
                 </div>
-                */}
 
             </div>
-
         </React.Fragment >
     );
 };
