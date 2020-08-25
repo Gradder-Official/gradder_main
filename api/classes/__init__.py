e180e348b938eb96eac423c26caf70b442847fcf
Alex Nikanov <alekseynikanov.21@gmail.com>
2020-08-26 02:45:13 +0300

    fix: :bug: Added SchoolConfig to the package imports in classes

--- api/classes/__init__.py
+++ api/classes/__init__.py
@@ -1,11 +1,13 @@
 # NOTE: the order is important to avoid the circular imports
+from .calendar_event import CalendarEvent
+from .schoolconfig import SchoolConfig
 from .user import User
 
 from .submission import Submission
 from .assignment import Assignment
 from .course import Course
 
-from .admin import Admin
 from .student import Student
 from .parent import Parent
 from .teacher import Teacher
+from .admin import Admin
