1417b8e111f7d5f7baf5ab6db332281f73d2a59a
Michael Wang <michaelwwang17@gmail.com>
2020-08-27 11:21:27 -0400

    styled event forms

--- client/src/pages/Teacher/Timetable.tsx
+++ client/src/pages/Teacher/Timetable.tsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState, Component } from "react";
 import { useForm } from "react-hook-form";
-import { Events } from "../../components/Interfaces"
+import { Events } from "../../components/Interfaces";
 
 import StudentSidebar from "../../components/TeacherSidebar";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import ReactModal from 'react-modal';
+import ReactModal from "react-modal";
 
 import "../../assets/styles/dashboard.css";
 import "../../assets/styles/timetable.css";
 import "../../assets/styles/modal.css";
 
 const TeacherTimetable = () => {
-
   const [eventList, setEventList] = useState<Array<Events>>([
-    { title: "", start: "", end: "", color: "", url: "" }
+    { title: "", start: "", end: "", color: "", url: "" },
   ]);
 
   function fetchCalendar() {
@@ -23,58 +22,55 @@ const TeacherTimetable = () => {
       .then((res) => res.json())
       .then((data) => {
         setEventList(data["data"]["events"]);
-        console.log(eventList)
+        console.log(eventList);
       });
   }
 
   // Fetches events from calendar
-  useEffect(() => { fetchCalendar() }, []);
+  useEffect(() => {
+    fetchCalendar();
+  }, []);
 
   const [requestErrors, setRequestErrors] = useState<string>();
   const { register, handleSubmit, errors } = useForm();
 
   // Adding an event
   const onSubmit = (data: Events) => {
-
     // Send data about event to be added to API
-    fetch('/api/teacher/calendar', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    fetch("/api/teacher/calendar", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     })
-      .then(async response => {
-
+      .then(async (response) => {
         const res = await response.json();
-        setEventList(res.events)
-        console.log(eventList)
+        setEventList(res.events);
+        console.log(eventList);
 
         // Check for error response
         if (!response.ok) {
           const error = (res && res.message) || response.status;
           return Promise.reject(error);
         }
-
       })
-      .catch(error => {
+      .catch((error) => {
         // Return errors
-        console.error('There was an error!', error);
-        setRequestErrors("Sorry, there was problem with creating an event!")
+        console.error("There was an error!", error);
+        setRequestErrors("Sorry, there was problem with creating an event!");
       });
-  }
+  };
 
   // Modal controls
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const [modalRemoveIsOpen, setRemoveIsOpen] = React.useState(false);
   function openModal() {
     setIsOpen(true);
-
   }
 
   function closeModal() {
     setIsOpen(false);
     fetchCalendar();
   }
-
 
   function openRemoveModal() {
     setRemoveIsOpen(true);
@@ -88,32 +84,28 @@ const TeacherTimetable = () => {
   // FUNCTION TO DELETE EVENT - to be implemented
   // Takes JSON of one key-value pair: { "title": title of event to be deleted }
   function deleteEvent(data: Events) {
-
-    fetch('/api/teacher/delete-calendar', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    fetch("/api/teacher/delete-calendar", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     })
-      .then(async response => {
-
+      .then(async (response) => {
         const res = await response.json();
-        setEventList(res["data"]["events"])
-        console.log(eventList)
+        setEventList(res["data"]["events"]);
+        console.log(eventList);
 
         // Check for error response
         if (!response.ok) {
           const error = (res && res.message) || response.status;
           return Promise.reject(error);
         }
-
       })
-      .catch(error => {
+      .catch((error) => {
         // Return errors
-        console.error('There was an error!', error);
-        setRequestErrors("Sorry, there was problem with deleting this event!")
+        console.error("There was an error!", error);
+        setRequestErrors("Sorry, there was problem with deleting this event!");
       });
   }
-
 
   return (
     <React.Fragment>
@@ -146,52 +138,35 @@ const TeacherTimetable = () => {
         <ReactModal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
-          contentLabel="Example Modal"
           closeTimeoutMS={500}
-          className="Modal"
           overlayClassName="Overlay"
-        >  <button className="closebutton" onClick={closeModal}>&times;</button>
+        >
+          {" "}
+          <button className="closebutton" onClick={closeModal}>
+            &times;
+          </button>
           <div className="eventForm">
-
             <form onSubmit={handleSubmit(onSubmit)}>
-
-              <div>
-                <label>
-                  Event Name:
-            </label>
+              <div className="eventInput">
+                <label>Event Name:</label>
                 {errors.eventName && <span>This field is required</span>}
-                <input
-                  name="title"
-                  ref={register({ required: true })}
-                />
+                <input name="title" ref={register({ required: true })} />
               </div>
 
-              <div>
-                <label>
-                  Start Time:
-            </label>
+              <div className="eventInput">
+                <label>Start Time:</label>
                 {errors.startTime && <span>This field is required</span>}
-                <input
-                  name="start"
-                  ref={register({ required: true })}
-                />
+                <input name="start" ref={register({ required: true })} />
               </div>
 
-              <div>
-                <label>
-                  End Time:
-            </label>
+              <div className="eventInput">
+                <label>End Time:</label>
                 {errors.endTime && <span>This field is required</span>}
-                <input
-                  name="end"
-                  ref={register({ required: true })}
-                />
+                <input name="end" ref={register({ required: true })} />
               </div>
 
-              <div>
-                <label>
-                  Color:
-            </label>
+              <div className="eventInput">
+                <label>Color:</label>
                 {errors.color && <span>This field is required</span>}
                 <select name="color" ref={register({ required: true })}>
                   <option value="red">Red</option>
@@ -204,47 +179,48 @@ const TeacherTimetable = () => {
                 </select>
               </div>
 
-              <div>
-                <label>
-                  URL (Optional):
-                  <input name="url" ref={register({ required: false })} />
-                </label>
+              <div className="eventInput">
+                <label></label>
+                URL (Optional):
+                <input name="url" ref={register({ required: false })} />
               </div>
-              <input className="submit-button" type="submit" onClick={closeModal}/>
+              <div className="eventFormButton">
+                <input
+                  className="submit-button"
+                  type="submit"
+                  onClick={closeModal}
+                />
+              </div>
             </form>
           </div>
         </ReactModal>
-
-
 
         <ReactModal
           isOpen={modalRemoveIsOpen}
           onRequestClose={closeRemoveModal}
-          contentLabel="Example Modal"
           closeTimeoutMS={500}
-          className="Modal"
           overlayClassName="Overlay"
         >
-          <button className="closebutton" onClick={closeRemoveModal}>&times;</button>
+          <button className="closebutton" onClick={closeRemoveModal}>
+            &times;
+          </button>
           <div className="eventForm">
-
             <form onSubmit={handleSubmit(deleteEvent)}>
-              <div>
-                <label>
-                  Event Name:
-            </label>
+              <div className="eventInput">
+                <label>Event Name:</label>
                 {errors.eventName && <span>This field is required</span>}
+                <input name="title" ref={register({ required: true })} />
+              </div>
+              <div className="eventFormButton">
                 <input
-                  name="title"
-                  ref={register({ required: true })}
+                  className="submit-button"
+                  type="submit"
+                  onClick={closeRemoveModal}
                 />
               </div>
-              <input className="submit-button" type="submit" onClick={closeRemoveModal}/>
             </form>
           </div>
         </ReactModal>
-
-
       </div>
     </React.Fragment>
   );
