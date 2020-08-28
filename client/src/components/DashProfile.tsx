c2f8c09fa71471659d2fa4fa8e6c46e9710bd6e9
unknown <varunch321@gmail.com>
2020-08-28 13:27:16 -0400

    Standardize colors across pages

--- client/src/components/DashProfile.tsx
+++ client/src/components/DashProfile.tsx
@@ -5,51 +5,52 @@ import createBrowserHistory from 'history/createBrowserHistory';
 
 const DashProfile: FunctionComponent<student | teacher> = ({ userName }) => {
 
-    const [logoutMessage, setLogoutMessage] = useState<string>();
-    const history = createBrowserHistory({forceRefresh:true});
+  const [logoutMessage, setLogoutMessage] = useState<string>();
+  const history = createBrowserHistory({ forceRefresh: true });
 
-    const logout = () => {
+  const logout = () => {
 
-        fetch('/api/auth/logout')
-        .then(res => res.json()).then(response => {
-            setLogoutMessage(response['flashes']);
-            console.log(logoutMessage)
-        })
+    fetch('/api/auth/logout')
+      .then(res => res.json()).then(response => {
+        setLogoutMessage(response['flashes']);
+        console.log(logoutMessage)
+      })
 
-        history.push("/auth/logout");
+    history.push("/auth/logout");
 
-    }
+  }
 
-    // Get current time in hours:minutes
-    const [hour, minute] = new Date().toLocaleTimeString().slice(0, 7).split(":");
-    const curTime = hour + ":" + minute;
+  // Get current time in hours:minutes
+  const [hour, minute] = new Date().toLocaleTimeString().slice(0, 7).split(":");
+  const curTime = hour + ":" + minute;
 
-    // Get day, month, date
-    const options = { weekday: "long", month: "long", day: "numeric" };
-    const curDate = new Date().toLocaleDateString(undefined, options);
+  // Get day, month, date
+  const options = { weekday: "long", month: "long", day: "numeric" };
+  const curDate = new Date().toLocaleDateString(undefined, options);
 
-    return (
-        <div className="dash-container profile">
-            <div className="profile-details">
-              <h2>Hello, {userName}</h2>
-              <img
-                src="https://www.iambetter.org/wp-content/uploads/2020/04/Albert_Einstein_1024x1024.jpg"
-                alt="Profile"
-                className="profile-pic"
-              />
-            </div>
-            <div className="profile-time">
-              <Link to="/" onClick={logout}>
-                <i className="material-icons-outlined">exit_to_app</i>
-              </Link>
-              <Link to="/dashboard">
-                <i className="material-icons-outlined">mail</i>
-              </Link>
-              <h1>{curTime}</h1>
-              <p>{curDate}</p>
-            </div>
-          </div>
-    )
+  return (
+    <div style={{backgroundColor: "rgb(90, 115, 226)"}} className="dash-container profile">
+      <div className="profile-details">
+        <h2>Hello, {userName}</h2>
+        <img
+          src="https://www.iambetter.org/wp-content/uploads/2020/04/Albert_Einstein_1024x1024.jpg"
+          alt="Profile"
+          className="profile-pic"
+          style={{backgroundColor: "rgb(76, 102, 212)"}}
+        />
+      </div>
+      <div className="profile-time">
+        <Link to="/" onClick={logout}>
+          <i className="material-icons-outlined">exit_to_app</i>
+        </Link>
+        <Link to="/dashboard">
+          <i className="material-icons-outlined">mail</i>
+        </Link>
+        <h1>{curTime}</h1>
+        <p>{curDate}</p>
+      </div>
+    </div>
+  )
 }
 
 export default DashProfile
